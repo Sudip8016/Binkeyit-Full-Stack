b6c12245a50b1de96c7883a771e8c09bcae102ba
Sudipta Kuila <sudiptakuila89@gmail.com>
2025-04-14 15:54:19 +0530

    first commit

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import { useGlobalContext } from '../provider/GlobalProvider'
+import { Link } from 'react-router-dom'
 
 const Success = () => {
   const { fetchCartItem, fetchOrder } = useGlobalContext()
