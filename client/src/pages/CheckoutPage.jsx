c44bfd5d8c67d43b96bc14e279683d05e897096c
Sudipta Kuila <sudiptakuila89@gmail.com>
2025-04-14 15:34:14 +0530

    first commit

--- client/src/pages/CheckoutPage.jsx
+++ client/src/pages/CheckoutPage.jsx
@@ -78,11 +78,6 @@ const CheckoutPage = () => {
             if(fetchOrder){
               fetchOrder()
             }
-            navigate('/success',{
-                state : {
-                  text : "Order"
-                }
-              })
         } catch (error) {
             AxiosToastError(error)
         }
